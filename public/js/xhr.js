3e5c5a69af9ef80ed759515042a5241c3fde8131
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-02-10 11:18:24 +0530

    modified xhr.js .
    extracted a function handleXhrRequest

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,88 +1,66 @@
-const sendStatusRequest = function(event) {
+const handleXhrRequest = function(url, method, data, callback) {
   const req = new XMLHttpRequest();
-  req.open('POST', '/setStatus');
   req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
+    callback(this.status, this.responseText, 'todoPage');
   };
-  const bucketId = event.target.className;
-  const taskId = event.target.id;
-  req.send(JSON.stringify({ bucketId, taskId }));
+  req.open(method, url);
+  req.send(JSON.stringify(data));
 };
 
 const sendSaveRequest = function() {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/saveTodo');
   const title = document.getElementById('title').value;
-  req.send(JSON.stringify({ title }));
+  handleXhrRequest('/saveTodo', 'POST', { title }, changeMainPageContent);
 };
+
 const sendEditTitleRequest = function(event) {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/editTitle');
   const title = event.target.innerText;
   const bucketId = event.target.id;
-  req.send(JSON.stringify({ title, bucketId }));
+  const data = { title, bucketId };
+  handleXhrRequest('/editTitle', 'POST', data, changeMainPageContent);
 };
 
 const sendDeleteBucketRequest = function(event) {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/deleteBucket');
   const bucketId = event.target.id;
-  req.send(JSON.stringify({ bucketId }));
+  const data = { bucketId };
+  handleXhrRequest('/deleteBucket', 'POST', data, changeMainPageContent);
 };
 
 const sendSaveNewTaskRequest = function(event) {
-  const req = new XMLHttpRequest();
   const bucketId = event.target.id;
   const inputBox = `#newTask${bucketId}[name="newTask"]`;
   const task = document.querySelector(inputBox).value;
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/saveNewTask');
-  req.send(JSON.stringify({ bucketId, task }));
+  const data = { task, bucketId };
+  handleXhrRequest('/saveNewTask', 'POST', data, changeMainPageContent);
+};
+const sendStatusRequest = function(event) {
+  const req = new XMLHttpRequest();
+  const bucketId = event.target.className;
+  const taskId = event.target.id;
+  const data = { bucketId, taskId };
+  handleXhrRequest('/setStatus', 'POST', data, changeMainPageContent);
 };
 
 const sendDeleteTaskRequest = function(event) {
   const classes = event.target.className;
-  const req = new XMLHttpRequest();
   const [bucketId] = classes.split(' ');
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/deleteTask');
   const taskId = event.target.id;
-  req.send(JSON.stringify({ bucketId, taskId }));
+  const data = { bucketId, taskId };
+  handleXhrRequest('/deleteTask', 'POST', data, changeMainPageContent);
 };
 
 const sendEditTaskRequest = function(event) {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/editTask');
   const taskId = event.target.id;
   const text = event.target.innerText;
   const classes = event.target.className;
   const [bucketId] = classes.split(' ');
-  req.send(JSON.stringify({ taskId, bucketId, text }));
+  const data = { taskId, bucketId, text };
+  handleXhrRequest('/editTask', 'POST', data, changeMainPageContent);
 };
 
 const sendSearchRequest = function(event) {
   const req = new XMLHttpRequest();
   const text = event.target.value;
   const searchBy = document.querySelector('.searchBy').dataset.searchby;
-  req.onload = function() {
-    changeMainPageContent(this.status, this.responseText, 'todoPage');
-  };
-  req.open('POST', '/search');
-  req.send(JSON.stringify({ text, searchBy }));
+  const data = { text, searchBy };
+  handleXhrRequest(`/search${searchBy}`, 'POST', data, changeMainPageContent);
 };
