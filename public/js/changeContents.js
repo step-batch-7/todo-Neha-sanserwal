187d4cbbf00d442753c0a8d44a4298ff2a316087
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-02-12 16:32:50 +0530

    [nehaa/rivu] created login and signup templates.
    created functions of creating signup templates which will be shown on
    homePage.
    
    login page on clicking sign in button takes to todo page,
    without any authentication.(authetication is still needs to be added).

--- public/js/changeContents.js
+++ public/js/changeContents.js
@@ -1,3 +1,33 @@
+const loadSignUpPage = function() {
+  const todoPage = document.querySelector('.todoPage');
+  todoPage.innerHTML = ` <div class="loginPage">
+        <h1> Sign Up </h1>
+        <div class="loginContainer">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <input type="password" placeholder="re-enter password" />
+          <button>SIGN UP</button>
+          <div>Already have an account <a href="#" onclick="loadLoginPage()">Sign In</a> here</div>
+        </div>
+      </div>`;
+};
+
+const loadLoginPage = function() {
+  const todoPage = document.querySelector('.todoPage');
+  todoPage.innerHTML = `<div class="loginPage">
+        <h1>Log In</h1>
+        <div class="loginContainer">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button onclick="serveTodo(event)">SIGN IN</button>
+          <div>
+            Don't have account
+            <a href="#" onclick="loadSignUpPage()">Sign Up</a> here
+          </div>
+        </div>
+      </div>`;
+};
+
 const changeMainPageContent = function(status, responseText, contentClass) {
   const Ok = 200;
   if (status === Ok) {
